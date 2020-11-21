54b246691e4c0ae415e13643fa313b27436a81c1
Gecko Uno <dominica.patterson@gmail.com>
2020-11-21 14:03:52 -0800

    Secrets Final Version - Google Auth

--- app.js
+++ app.js
@@ -4,33 +4,89 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+//This tells our app to use the session package required above
+app.use(session({
+  //you can add any long string here, just remember it!
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
 
-//we will add the encrypt package as a pluggin for the mongoose schema
+//here we tell our app to use passport & to initialize the passport package
+app.use(passport.initialize());
+//this is where we tell our app to use passport when managing sessions
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+//add this b/c of deprecation warning in console
+mongoose.set("useCreateIndex", true);
+
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
 });
-
-//We cut out the secret const to move it to the .env file & we access it with process.env
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
+//tap into your schema & add plugin
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+ 
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+//Copied from passport-google-oauth20 Configure Strategy
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"  //we will no longer retrieve user info from Google+ as it is deprecated
+  }, //call back function below. Note: findOrCreate is not a reall method but pseudocode. You add your desired Mongo method in its place or use Mongoose findOrCrete plugin on NPM!
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res) {
     res.render("home");
 });
+
+//here we say use passport to authenticate our user with the google strategy
+app.get("/auth/google",
+    passport.authenticate("google", { scope: ["email", "profile"] } 
+));
+
+app.get("/auth/google/secrets",
+    passport.authenticate("google", {
+        successRedirect: "/secrets",
+        failureRedirect: "/login"
+}));
 
 app.get("/login", function(req, res) {
     res.render("login");
@@ -40,41 +96,85 @@ app.get("/register", function(req, res) {
     res.render("register");
 });
 
-app.post("/register", function(req, res) {
-    //This is where we create the brand new user using the info passed from the registration form.
-    const newUser = new User ({
-        email: req.body.username,
-        password: req.body.password
-    });
-
-    newUser.save(function(err) {
+app.get("/secrets", function (req, res) {
+    User.find({"secret": {$ne: null}}, function(err, foundUsers) {
         if (err) {
             console.log(err);
         } else {
-            //very important: we will ONLY render this page from the register/login routes. There is no app.get for this one!
-            res.render("secrets");
+            if (foundUsers) {
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
         }
     });
+});
+
+app.get("/submit", function(req, res) {
+    if (req.isAuthenticated()) {
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+app.post("/register", function(req, res) {
+    //this is our middleman to handle creating & saving our users
+    User.register({username: req.body.username}, req.body.password, function(err, result) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local") (req, res, function() {
+                res.redirect("/secrets");
+            });
+        }
+    });
+    
 });
 
 app.post("/login", function (req, res) {
-    //This is where will will check if their details against our DB
-    const username = req.body.username;
-    const password = req.body.password;
+    
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-    User.findOne({email: username}, function(err, foundUser) {
-        if (!err) {
-            if (foundUser) {
-                if (password === foundUser.password) {
-                    res.render("secrets")
-                }
-            }
-        } else {
+    //passport gives us a login() function on req
+    req.login(user, function(err) {
+        if (err) {
             console.log(err);
+        } else {
+            passport.authenticate("local") (req, res, function() {
+                res.redirect("/secrets");
+            });
         }
     });
+
 });
 
+app.post("/submit", function(req, res) {
+    const submittedSecret = req.body.secret;
+    //now find the user in our DB & save thier secret to their file:
+    console.log(req.user.id);
+
+    User.findById(req.user.id, function(err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function() {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
+
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000.")
@@ -83,9 +183,7 @@ app.listen(3000, function() {
 
 /*
 
-If a doc is not found, it's not an error. 
-The foundUser from findOne() will be null so 
-you need to capture that in an else statement.
+
 
 */
 
